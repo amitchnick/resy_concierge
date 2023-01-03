3157e57181c783fadbbf170105c2ab8262d887e0
Alexis Mitchnick <alexismitchnick@gmail.com>
2023-01-03 18:20:57 -0500

    feat: Don Angie

--- constants.py
+++ constants.py
@@ -3,5 +3,8 @@ RESY_URL = "https://api.resy.com/"
 RESTAURANT_TO_VENUE_ID = {
     "Rosemary's": 2,
     "I Sodi": 443,
-    "Laser Wolf": 58848
+    "Laser Wolf": 58848,
+    "Don Angie": 1505,
+    "4 Charles Prime Rib": 834,
+    "Bistecca": 3207
 }
